b8a38f52f02443c1dd8d00f5be1cd84bc6ec0f1e
Andrew <21085039+asmarsh@users.noreply.github.com>
2022-02-03 22:11:33 -0500

    Changes from Chapter 8

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -12,10 +12,11 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
-        public ViewResult Index(int productPage = 1)
+        public ViewResult Index(string category, int productPage = 1)
         => View(new ProductsListViewModel
         {
             Products = repository.Products
+        .Where(p => category == null || p.Category == category)
         .OrderBy(p => p.ProductID)
         .Skip((productPage - 1) * PageSize)
         .Take(PageSize),
@@ -23,8 +24,12 @@ namespace SportsStore.Controllers
             {
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count()
-            }
+                TotalItems = category == null ?
+        repository.Products.Count() :
+       repository.Products.Where(e =>
+        e.Category == category).Count()
+            },
+            CurrentCategory = category
         });
     }
 }
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
-
 namespace SportsStore
 {
     public class Startup
@@ -30,18 +29,30 @@ namespace SportsStore
                 Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting();
             app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("catpage",
+                "{category}/Page{productPage:int}",
+                new { Controller = "Home", action = "Index" });
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                new { Controller = "Home", action = "Index", productPage = 1 });
+                endpoints.MapControllerRoute("category", "{category}",
+                new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapControllerRoute("pagination",
                 "Products/Page{productPage}",
-                new { Controller = "Home", action = "Index" });
+                new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
             SeedData.EnsurePopulated(app);
         }
