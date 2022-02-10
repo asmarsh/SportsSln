da112bdab24fdcde9fbb11dc4204dd334914eccd
Andrew <21085039+asmarsh@users.noreply.github.com>
2022-02-09 20:24:40 -0500

    Done with chapter 10

--- SportsStore/Models/IStoreRepository.cs
+++ SportsStore/Models/IStoreRepository.cs
@@ -1,8 +1,15 @@
 ﻿using System.Linq;
+
 namespace SportsStore.Models
 {
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product p);
+
+        void CreateProduct(Product p);
+
+        void DeleteProduct(Product p);
     }
 }
